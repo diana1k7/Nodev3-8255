ae822dec095332786282724be927bfc04a908116
diana1k7 <52952527+diana1k7@users.noreply.github.com>
2024-01-13 17:47:27 -0600

    Update devio8255.h

--- devio8255.h
+++ devio8255.h
@@ -14,6 +14,33 @@ extern Adafruit_MCP23X17 mcp;
 #define port1cs mcp.pa5
 #define port2reset mcp.pa6
 #define port2cs mcp.pa7
+#define A0 mcp.p2
+#define A1 mcp.p3
+#define RD mcp.pa0
+#define WR mcp.p1
+
+int portReset[2]= { port1reset, port2reset } ;
+int portCS[2] = { port1cs, port2cs } ;
+int addr1[2] = { A0, A1 } ;
+
+#define rCtrl 0
+#define rPortA 1
+#define rPortB 2
+#define rPortC 3
+
 
 void wrCtrlReg(int portId,  int data1){
-  
+  int b1;
+  int tmp = rCtrl;
+
+// select port chip
+digitalWrite(portCS[portId], HIGH);
+
+// select port register
+for (int i=0; i<=1; i++){
+  b1 = tmp & 0x1;
+  digitalWrite(addr1[i], b1);
+  tmp = tmp << 1;
+}
+
+
